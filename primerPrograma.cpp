99129681d465bc750c9b662691958526c8924fd0
Jrodri-19 <145525087+Jrodri-19@users.noreply.github.com>
2024-04-26 21:11:42 -0500

    Add files via upload

---
+++ primerPrograma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int n = 0;
+	scanf("%d", &n); // Leemos un entero desde la entrada estándar
+	
+	int* pn = nullptr;
+	pn = &n;
+	
+	printf("%d", *pn); // Imprimimos el valor de n usando printf
+	
+	return 0;
+}
