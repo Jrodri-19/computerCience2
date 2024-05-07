146d668fc3b029d0982db5aa6d916f0014b39bcc
Jrodri-19 <145525087+Jrodri-19@users.noreply.github.com>
2024-05-06 22:58:35 -0500

    Update 1PunteroAFunciones.cpp

--- clase2/1PunteroAFunciones.cpp
+++ clase2/1PunteroAFunciones.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+void miFuncion(int parametro) {
+	// Código de la función
+	std::cout<<"Imprime";
+}
 
+void mf(int c){
+	// Código de la función
+	std::cout<<c;
+}
+
+int main() {
+	void (*foo)(int); // Declaración de un puntero a una función que toma un parámetro entero y no devuelve valor
+	foo = miFuncion; // Asignando la dirección de la función miFuncion al puntero foo
+	foo(10); // Llamando a la función a través del puntero
+	foo = mf; // Asignando la dirección de la función miFuncion al puntero foo
+	foo(3); // Llamando a la función a través del puntero
+	return 0;
+}
